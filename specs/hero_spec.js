ad5c4556ee215553e054080ccc71a2671dce5458
Jamie Sime <jamiesime@gmail.com>
2017-11-30 20:59:16 +0000

    name test passed

--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -1,2 +1,14 @@
 var assert = require("assert");
-var hero = require("../hero.js");
+var Hero = require("../hero.js");
+
+describe("Hero", (function(){
+
+  beforeEach(function(){
+    hero1 = new Hero("Faye");
+  });
+
+  it("should have a name", function(){
+      assert.strictEqual(hero1.name, "Faye");
+  });
+
+}));
