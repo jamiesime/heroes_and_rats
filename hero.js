4e841a7c28aef333a514bcedcec718e2007c3dd3
Jamie Sime <jamiesime@gmail.com>
2017-11-30 21:03:29 +0000

    passing favfood, passing health at start

--- hero.js
+++ hero.js
@@ -1,6 +1,8 @@
 
-var Hero = function (name) {
+var Hero = function (name, favFood) {
   this.name = name;
+  this.favFood = favFood;
+  this.health = 20;
 };
 
 module.exports = Hero;
--- specs/hero_spec.js
+++ specs/hero_spec.js
@@ -4,11 +4,19 @@ var Hero = require("../hero.js");
 describe("Hero", (function(){
 
   beforeEach(function(){
-    hero1 = new Hero("Faye");
+    hero1 = new Hero("Faye", "Sprouts");
   });
 
   it("should have a name", function(){
       assert.strictEqual(hero1.name, "Faye");
+  });
+
+  it("should have a fav food", function(){
+    assert.strictEqual(hero1.favFood, "Sprouts");
+  });
+
+  it("should have 20 health at start", function(){
+    assert.strictEqual(hero1.health, 20);
   });
 
 }));
